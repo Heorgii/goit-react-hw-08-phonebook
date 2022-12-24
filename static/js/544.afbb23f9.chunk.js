49c7cb3ef331f6743968d9ac948842386fb19292
Heorgii <103379960+Heorgii@users.noreply.github.com>
2022-12-24 17:13:21 +0000

    Deploying to gh-pages from @ Heorgii/goit-react-hw-08-phonebook@546aca783b9623d16d39244d8482846f4945b659 🚀

---
+++ static/js/544.afbb23f9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{9544:function(e,t,o){o.r(t);var r=o(184);t.default=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h1",{children:"Welcome to Phonebook"}),(0,r.jsx)("p",{children:"If you are register just log in, but if you don't have account yet registraion."})]})}}}]);
+//# sourceMappingURL=544.afbb23f9.chunk.js.map
