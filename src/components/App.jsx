0aaa819121907d073d888aad286d97b616d173e4
Heorgii <rushchakheorgii@gmail.com>
2022-12-27 19:48:29 +0300

    some fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,71 @@
-import { lazy } from "react";
-import { useDispatch } from "react-redux";
-import { useAuth } from "hooks/useAuth";
-import { useEffect } from "react";
-import { refreshUser } from "redux/auth/operations";
-import { Route, Routes } from "react-router-dom";
-import SharedLayout from "./SharedLayout.jsx";
+// import { lazy } from "react";
+// import { useDispatch } from "react-redux";
+// import { useAuth } from "hooks/useAuth";
+// import { useEffect } from "react";
+// import { refreshUser } from "redux/auth/operations";
+// import { Route, Routes } from "react-router-dom";
+// import SharedLayout from "./SharedLayout.jsx";
 // import { RestrictedRoute } from "utils/RestrictedRoute.jsx";
 // import { PrivateRoute } from "utils/PrivateRoute.jsx";
 // import css from './App.module.css';
 
-const HomePage = lazy(() => import('../pages/Home/Home'));
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
+
+// const HomePage = lazy(() => import('../pages/Home/Home'));
 // const RegisterPage = lazy(() => import('../pages/Register/Register'));
 // const LoginPage = lazy(() => import('../pages/LogIn/LogIn'));
 // const ContactsPage = lazy(() => import('../pages/Contacts/Contacts'));
 
 
 const App = () => {
-  const dispatch = useDispatch();
-  const isRefreshing = useAuth();
-  console.log(isRefreshing);
+  // const dispatch = useDispatch();
+  // const isRefreshing = useAuth();
+  // console.log(isRefreshing);
 
-  useEffect(() => {
-    dispatch(refreshUser())
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(refreshUser())
+  // }, [dispatch]);
 
-  return isRefreshing ? (
-    <b>Refreshing user...</b>
-  ) : (
+  return (
     <div>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<HomePage />} />
-          {/* <Route path="/login"
-            element={
-              <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
-            }
-          />
-
-          <Route path="/register "
-            element={
-              <RestrictedRoute redirectTo="/contacts" component={<RegisterPage />} />
-            }
-          />
-
-          <Route path="/contacts "
-            element={
-              <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
-            }
-          /> */}
-        </Route>
-      </Routes>
+      <h1 >Phonebook</h1>
+      <ContactForm />
+      <h2 >Contacts</h2>
+      <Filter />
+      <ContactList />
     </div>
   );
+
+  // return isRefreshing ? (
+  //   <b>Refreshing user...</b>
+  // ) : (
+  //   <div>
+  //     <Routes>
+  //       <Route path="/" element={<SharedLayout />}>
+  //         <Route index element={<HomePage />} />
+  //         {/* <Route path="/login"
+  //           element={
+  //             <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+  //           }
+  //         />
+
+  //         <Route path="/register "
+  //           element={
+  //             <RestrictedRoute redirectTo="/contacts" component={<RegisterPage />} />
+  //           }
+  //         />
+
+  //         <Route path="/contacts "
+  //           element={
+  //             <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+  //           }
+  //         /> */}
+  //       </Route>
+  //     </Routes>
+  //   </div>
+  // );
 
 
 };
--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import App from 'components/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux/es/exports';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import { store } from 'redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-        <BrowserRouter basename='/goit-react-hw-08-phonebook/'>
+        {/* <BrowserRouter basename='/goit-react-hw-08-phonebook/'> */}
           <App />
-        </BrowserRouter>
+        {/* </BrowserRouter> */}
     </Provider>
   </React.StrictMode>
 );
