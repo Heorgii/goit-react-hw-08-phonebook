7716422444bae36a294b651709ab7d3e7a22d1d1
Heorgii <rushchakheorgii@gmail.com>
2022-12-29 21:22:07 +0300

    Update AuthNav.jsx

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom";
 const AuthNav = () => {
     return (
         <div>
-            <NavLink to={'/register'}>
+            <NavLink to="/register">
                 Register
             </NavLink>
 
-            <NavLink to={'/login'}>
+            <NavLink to="/login">
                 Log In
             </NavLink>
         </div>
