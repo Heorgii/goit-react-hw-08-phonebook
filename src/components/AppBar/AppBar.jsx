bd0a4698d63f8509dbea100fb29b6a73e73fe52a
Heorgii <rushchakheorgii@gmail.com>
2022-12-30 12:10:45 +0300

    NavAuth

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,16 +1,16 @@
-// import AuthNav from "components/AuthNav/AuthNav";
 // import Navigation from "components/Navigation/Navigation";
+import NavAuth from "components/NavAuth/NavAuth";
 import UserMenu from "components/UserMenu/UserMenu";
 // import { useAuth } from "hooks/useAuth";
 
 
 const AppBar = () => {
     // const {isLoggedIn} = useAuth();
-    
+
     return (
         <header>
             <UserMenu />
-            {/* <AuthNav /> */}
+            <NavAuth />
             {/* <Navigation /> */}
             {/* {isLoggedIn ? <UserMenu /> : <AuthNav />} */}
         </header>
---
+++ src/components/NavAuth/NavAuth.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const NavAuth = () => {
+    <div>
+        <Link>
+            Register
+        </Link >
+
+        <Link>
+            Log In
+        </Link >
+    </div>
+}
+
+export default NavAuth;
